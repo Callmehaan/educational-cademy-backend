f065a454ca3c07cea0f73e65748c611bb10211ac
Mahan <121290805+Callmehaan@users.noreply.github.com>
2024-04-30 08:31:56 +0330

    Add files via upload

---
+++ routes/v1/comment.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const commentController = require("./../../controllers/v1/comment");
+const authMiddleware = require("./../../middlewares/auth");
+const isAdminMiddleware = require("./../../middlewares/isAdmin");
+
+const router = express.Router();
+
+router
+    .route("/")
+    .post(authMiddleware, commentController.create)
+    .get(authMiddleware, isAdminMiddleware, commentController.getAll);
+
+router
+    .route("/:id")
+    .delete(authMiddleware, isAdminMiddleware, commentController.remove);
+
+router
+    .route("/:id/accept")
+    .put(authMiddleware, isAdminMiddleware, commentController.accept);
+
+router
+    .route("/:id/reject")
+    .put(authMiddleware, isAdminMiddleware, commentController.reject);
+
+router
+    .route("/:id/answer")
+    .post(authMiddleware, isAdminMiddleware, commentController.answer);
+
+module.exports = router;
